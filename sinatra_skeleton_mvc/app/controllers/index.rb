eea23445ca946d892a0b43b7d02c79e2f3ef80ae
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-07 12:18:36 -0800

    added controller test models

--- sinatra_skeleton_mvc/app/controllers/index.rb
+++ sinatra_skeleton_mvc/app/controllers/index.rb
@@ -1,8 +1,21 @@
 get '/' do
   # Look in app/views/index.erb
+  @survey = Survey.all
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+get '/survey/new' do
+  erb :new_survey
 end
+
+post '/survey' do
+  survey = Survey.create(name: params[:title])
+  question = Question.create(text: params[:question])
+  choice1 = Choice.create(text: params[:choice1])
+  choice2 = Choice.create(text: params[:choice2])
+  survey.questions << question
+  question.choices << choice1
+  question.choices << choice2
+  redirect '/'
+end
+# make restful
