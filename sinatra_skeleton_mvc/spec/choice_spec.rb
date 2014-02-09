d9266540a07e4f0ae1ba6d64bc1f4f181f983a15
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-09 14:45:21 -0800

    Adds tests for choice model

---
+++ sinatra_skeleton_mvc/spec/choice_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Choice do
+
+  before :all do
+    Choice.delete_all
+    Question.delete_all
+  end
+
+  after :all do
+    Choice.delete_all
+    Question.delete_all
+  end
+
+  describe "choice creation" do
+
+    before :all do
+      @question = Question.create(text: "This is a question")
+      @choice = Choice.create(text: "This is a choice", question_id: @question.id)
+    end
+
+    it "includes text content" do
+      expect(@choice.text).not_to eq(nil)
+    end
+
+    it "belongs to a question" do
+      expect(@choice.question_id).to eq(@question.id)
+    end
+
+    it "should have a default count of 0" do
+      expect(@choice.count).to eq(0)
+    end
+  end
+end
+
+
+# class CreateChoices < ActiveRecord::Migration
+#   def change
+#     create_table :choices do |t|
+#       t.string :text
+#       t.integer :count, :default => 0
+#       t.integer :question_id
+#     end
+#   end
+# end
