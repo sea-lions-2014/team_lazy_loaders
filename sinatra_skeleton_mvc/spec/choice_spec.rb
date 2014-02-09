924e23933bfddac0cc33aa59ff25fc5ea7448989
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-09 14:52:53 -0800

    Removes comments from choice_spec

--- sinatra_skeleton_mvc/spec/choice_spec.rb
+++ sinatra_skeleton_mvc/spec/choice_spec.rb
@@ -32,14 +32,3 @@ describe Choice do
     end
   end
 end
-
-
-# class CreateChoices < ActiveRecord::Migration
-#   def change
-#     create_table :choices do |t|
-#       t.string :text
-#       t.integer :count, :default => 0
-#       t.integer :question_id
-#     end
-#   end
-# end
