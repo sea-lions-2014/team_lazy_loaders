fa20e395ec202552a4df81ed1c946b842b1c5675
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-09 10:58:21 -0800

    Add two user authentication tests

--- sinatra_skeleton_mvc/spec/user_spec.rb
+++ sinatra_skeleton_mvc/spec/user_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe User do
 
+  before :all do
+    User.delete_all
+  end
+
+  after :all do
+    User.delete_all
+  end
+
   describe "user creation" do 
     before :all do
       @user = User.new(username:'bob')
@@ -28,6 +36,22 @@ describe User do
     it "should have a unique username" do
       bob2 = User.new(username:'bob')
       bob2.valid?.should_not == true
+    end
+  end
+
+  describe "user authentication" do
+    before :all do
+      @user = User.new(username:'bob')
+      @user.password = 'bob'
+      @user.save
+    end
+
+    it "should reject an incorrect password" do
+      @user.password_hash.should_not == 'notbob'
+    end
+
+    it "should authorize someone with the correct password" do
+      @user.password_hash.should == 'bob'
     end
   end
 end
